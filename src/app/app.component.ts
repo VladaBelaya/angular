beeefab279d63a0898aee3d9e8203ed009d620eb
VladaBelaya <vlada_chvanova@mail.ru>
2022-09-01 18:01:44 +0300

    create a new element on the server and complete project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {IProduct} from './modules/product';
-import {ProductsService} from "./services/products.service";
-import {Observable, tap} from "rxjs";
-import {ModalService} from "./services/modal.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'my first angular project'
-  // products: IProduct[] = []
-  loading = false
-  products$: Observable<IProduct[]>
-  term = ''
+export class AppComponent {
 
-  constructor(
-    private ProductsService: ProductsService,
-    public modalService: ModalService
-    ) {
-  }
-  ngOnInit(): void {
-    this.loading = true
-    this.products$ = this.ProductsService.getAll().pipe(
-      tap(() => this.loading = false)
-    )
-    // this.ProductsService.getAll().subscribe(products => {
-    //   this.products = products
-    //   this.loading = false
-    // })
-  }
 }
